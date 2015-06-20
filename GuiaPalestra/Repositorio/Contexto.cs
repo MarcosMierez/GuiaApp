9198aa51e4c51358cce09efac276ae133602eaf6
Marcos Vinicius <ka-ladze@hotmail.com>
2015-06-20 10:30:08 -0300

    comitando connection string

--- GuiaPalestra/Repositorio/Contexto.cs
+++ GuiaPalestra/Repositorio/Contexto.cs
@@ -6,15 +6,22 @@ using System.Web;
 using Dapper;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace GuiaPalestrasOnline.Repositorio
 {
     public class Contexto
     {
+        public static string getConnectionString()
+        {
+            return ConfigurationManager.AppSettings.Get("MYSQL_CONNECTION_STRING") ??
+                   "Database=palestrasonline;Data Source=localhost;User Id=root; Password=root";
+        }
         public Contexto()
         {
             SqlBd.Open();
         }
-        public  MySqlConnection SqlBd = new MySqlConnection("Database=palestrasonline;Data Source=localhost;User Id=root; Password=root") ?? ;
+
+        public MySqlConnection SqlBd = new MySqlConnection(getConnectionString());
     }
 }
